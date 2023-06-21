f1b8307d12283f6ba9f7cdc9036892e7b2680b32
Yulia <yuliagub07@gmail.com>
2023-06-21 12:52:00 +0300

    Rate Added

--- src/app/item/item-detail.component.ts
+++ src/app/item/item-detail.component.ts
@@ -11,17 +11,33 @@ import { Movie } from './models/Movie'
 
 export class ItemDetailComponent implements OnInit {
   movie: Movie;
+  highlights : number;
 
   constructor(private apiService: ApiService,
               private route: ActivatedRoute) {}
 
     ngOnInit(): void {
+    this.highlights = 0;
     const id = this.route.snapshot.params.id;
-    this.apiService.getMovie(id).subscribe(
-      (data:Movie) => {
-        this.movie = data;
-      },
-      error => console.log(error)
-    )
+    this.getDetails(id);
   }
+
+    setHighlight(rate :number){
+      this.highlights = rate;
+    }
+    rateClicked(){
+      this.apiService.rateMovie(this.highlights,this.movie.id).subscribe(
+        date => this.getDetails(this.movie.id),
+        error => console.log(error)
+      )
+    }
+
+    getDetails(id : number){
+      this.apiService.getMovie(id).subscribe(
+        (data:Movie) => {
+          this.movie = data;
+        },
+        error => console.log(error)
+      )
+    }
 }
