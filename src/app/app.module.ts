57ad6d4715481a3f7c61a071f1b0b82745a2d812
Yulia <yuliagub07@gmail.com>
2023-06-26 19:37:13 +0300

    Auth Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptModule } from '@nativescript/angular'
 import { NativeScriptHttpClientModule } from '@nativescript/angular' 
 import { HttpClientModule } from '@angular/common/http';
+import { NativeScriptFormsModule } from '@nativescript/angular';
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ItemsComponent } from './item/items.component'
 import { ItemDetailComponent } from './item/item-detail.component'
 import { MovieFormComponent } from './movieForm/movieForm.component'
-import { NativeScriptFormsModule } from '@nativescript/angular';
+import {AuthComponent} from './auth/auth.component'
+
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { NativeScriptFormsModule } from '@nativescript/angular';
             NativeScriptHttpClientModule,HttpClientModule,
             NativeScriptFormsModule],
   declarations: [AppComponent, ItemsComponent, 
-                ItemDetailComponent,MovieFormComponent,],
+                ItemDetailComponent,MovieFormComponent,
+                AuthComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
 })
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'auth',
+	templateUrl: './auth.component.html',
+	styleUrls: ['./auth.component.css']
+})
+
+export class AuthComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() { }
+}
